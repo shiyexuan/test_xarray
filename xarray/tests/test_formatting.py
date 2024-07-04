502b236f9116acbfff92ac59bddcac0ef46ffdce
shiyexuan <shiyexuan@bytedance.com>
2024-07-04 11:13:29 +0000

    xarray4248

--- xarray/tests/test_formatting.py
+++ xarray/tests/test_formatting.py
@@ -1,5 +1,6 @@
 import sys
 from textwrap import dedent
+from xarray.core.npcompat import IS_NEP18_ACTIVE
 
 import numpy as np
 import pandas as pd
@@ -390,6 +391,42 @@ class TestFormatting:
 
         assert actual == expected
 
+@pytest.mark.skipif(not IS_NEP18_ACTIVE, reason="requires __array_function__")
+def test_inline_variable_array_repr_custom_repr():
+    class CustomArray:
+        def __init__(self, value, attr):
+            self.value = value
+            self.attr = attr
+
+        def _repr_inline_(self, width):
+            formatted = f"({self.attr}) {self.value}"
+            if len(formatted) > width:
+                formatted = f"({self.attr}) ..."
+
+            return formatted
+
+        def __array_function__(self, *args, **kwargs):
+            return NotImplemented
+
+        @property
+        def shape(self):
+            return self.value.shape
+
+        @property
+        def dtype(self):
+            return self.value.dtype
+
+        @property
+        def ndim(self):
+            return self.value.ndim
+
+    value = CustomArray(np.array([20, 40]), "m")
+    variable = xr.Variable("x", value)
+
+    max_width = 10
+    actual = formatting.inline_variable_array_repr(variable, max_width=10)
+
+    assert actual == value._repr_inline_(max_width)
 
 def test_set_numpy_options():
     original_options = np.get_printoptions()
